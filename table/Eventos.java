b5d618bb602c7a061bbedddb1f3232703b0e9788
davs22 <mdiirr2003@gmail.com>
2025-04-16 23:47:47 -0300

    Adicao_de_classes

---
+++ table/Eventos.java
@@ -0,0 +1,52 @@
+package table;
+
+public class Eventos {
+    private String nome;
+    private String descricao;
+    private Integer data;
+    private String local;
+    private Palestrante palestrantes;
+    
+    public Eventos() {
+    }
+
+    public Eventos(String nome, String descricao, Integer data, String local, Palestrante palestrantes) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.data = data;
+        this.local = local;
+        this.palestrantes = palestrantes;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    public Integer getData() {
+        return data;
+    }
+    public void setData(Integer data) {
+        this.data = data;
+    }
+    public String getLocal() {
+        return local;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    public Palestrante getPalestrantes() {
+        return palestrantes;
+    }
+    public void setPalestrantes(Palestrante palestrantes) {
+        this.palestrantes = palestrantes;
+    }
+ 
+    
+}
