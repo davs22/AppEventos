c03749630f5ec24430fe0e444a1951a321ff5186
okanjota <jaimeejunioorr@gmail.com>
2025-04-21 17:42:04 -0300

    AssertsEquals

--- test/PalestraTest.java
+++ test/PalestraTest.java
@@ -1,12 +1,18 @@
+import java.beans.Transient;
 import java.time.LocalTime;;
 
 public class PalestraTest{
 
     @Test
     void criarPalestracomHorário() {
-    Palestra palestra = new Palestra("Java Avançado", LocalTime.of(10,0), LocalTime.of(12,0));
-    assertEquals("Java Avançado". palestra.getTitulo());
-    assertEquals(LocalTime.of(10,0),palestra.getInicio());
-    assertEquals(LocalTime.of(12,0),palestra.getFim());
+      Palestra palestra = new Palestra("Java Avançado", LocalTime.of(10,0), LocalTime.of(12,0));
+         assertEquals("Java Avançado". palestra.getTitulo());
+         assertEquals(LocalTime.of(10,0),palestra.getInicio());
+         assertEquals(LocalTime.of(12,0),palestra.getFim());
     }
+    
+    @Test
+    void LancarHorárioVálido {
+      assertThrows(IlegalArgumentException.class, () -> new Palestra("Spring Boot", LOcalTime));
+   }
 }  
