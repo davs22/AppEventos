c9c397d16fa2ad24bee2034e8e7c2d880a495844
okanjota <jaimeejunioorr@gmail.com>
2025-04-21 18:32:54 -0300

    Update PalestraTest.java
    mudança de ponto para vírgula

--- test/PalestraTest.java
+++ test/PalestraTest.java
@@ -6,7 +6,7 @@ public class PalestraTest{
     @Test
     void criarPalestracomHorário() {
       Palestra palestra = new Palestra("Java Avançado", LocalTime.of(10,0), LocalTime.of(12,0));
-         assertEquals("Java Avançado". palestra.getTitulo());
+         assertEquals("Java Avançado", palestra.getTitulo());
          assertEquals(LocalTime.of(10,0),palestra.getInicio());
          assertEquals(LocalTime.of(12,0),palestra.getFim());
     }
