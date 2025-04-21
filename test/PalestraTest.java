b451519c3e36864262d37782b2a76106705435ba
okanjota <jaimeejunioorr@gmail.com>
2025-04-21 17:32:31 -0300

    Declaração

--- test/PalestraTest.java
+++ test/PalestraTest.java
@@ -3,5 +3,10 @@ import java.time.LocalTime;;
 public class PalestraTest{
 
     @Test
-    void criarPalestra 
-}
+    void criarPalestracomHorário() {
+    Palestra palestra = new Palestra("Java Avançado", LocalTime.of(10,0), LocalTime.of(12,0));
+    assertEquals("Java Avançado". palestra.getTitulo());
+    assertEquals(LocalTime.of(10,0),palestra.getInicio());
+    assertEquals(LocalTime.of(12,0),palestra.getFim());
+    }
+}  
