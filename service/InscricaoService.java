c6b31b98c65008fcfe800dad294aeef8b30b20ea
Edu-1234 <dudu.miguel.karla.ganso@gmail.com>
2025-04-22 00:09:29 -0300

    Adicionado a classe inscricaoService.java

--- service/InscricaoService.java
+++ service/InscricaoService.java
@@ -1,5 +1,34 @@
 package service;
 
-public class InscricaoService {
+import dao.InscricaoDao;
+import java.util.List;
+import table.Inscricao;
+import util.SQLiteConnection;
 
+public class InscricaoService {
+    private InscricaoDao dao;
+
+    public InscricaoService() {
+        this.dao = new InscricaoDao(new SQLiteConnection());
+    }
+
+    public String inscreverParticipante(int idParticipante, int idEvento) {
+        return dao.inscreverParticipante(idParticipante, idEvento);
+    }
+
+    public List<Inscricao> listarInscricoesPorParticipante(int idParticipante) {
+        return dao.listarInscricoesPorParticipante(idParticipante);
+    }
+
+    public boolean verificarInscricao(int idParticipante, int idEvento) {
+        return dao.verificarInscricao(idParticipante, idEvento);
+    }
+
+    public String excluirInscricao(int idParticipante, int idEvento) {
+        return dao.excluirInscricao(idParticipante, idEvento);
+    }
+
+    public String solicitarCertificado(int idParticipante, int idEvento) {
+        return dao.solicitarCertificado(idParticipante, idEvento);
+    }
 }
