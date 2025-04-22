399b6d566a12a0ba746ed2ffa566e5feea4324d1
okanjota <jaimeejunioorr@gmail.com>
2025-04-22 17:52:40 -0300

    Create InscriçãoTeste.java
    Finalizando Inscrição Teste.java.java

---
+++ test/InscriçãoTeste.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Asseetions;
+import table.Insxricao;
+
+public class Inscricao {
+
+    @Test
+    void confirmarInscricaoComSucesso() {
+      Inscricao inscricao = new Inscricao ();
+      AssertTrue(inscricao.inscricaoComSucesso());
+}
+
+    @Test
+    void lancarExcecoesSeEventoOuParticipanteForNulo() {
+    assertThrows(IllegalArgumentException.class, () -> new Inscricao)
+    assertThrows(IllegalArgumentException.class, () -> new Inscricao)
+  }
+}
