90e813134b25ac0e243b44b9c3de923be95ab0da
davs22 <mdiirr2003@gmail.com>
2025-04-23 23:16:26 -0300

    teste

--- StartApp.java
+++ StartApp.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import service.EventosService;
 import service.InscricaoService;
 import service.ParticipanteService;
+import table.Eventos;
 import table.Participante;
 
 public class StartApp {
@@ -33,22 +34,41 @@ public class StartApp {
         
         //ps.inserir("Matheus", "M", "matheus@gmail.com", "(27) 99563224");
 
-        //EventosService es = new EventosService();
+        EventosService es = new EventosService();
 
+        try {
+            List<Eventos> eventos = es.listarEventos();
+            
+            for (Eventos e : eventos) {
+                System.out.println("ID: " + e.getId());
+                System.out.println("Nome: " + e.getNome());
+                System.out.println("Descrição: " + e.getDescricao());
+                System.out.println("Data: " + e.getData());
+                System.out.println("Local: " + e.getLocal());
+                System.out.println("Palestrante ID: " + e.getPalestranteId());
+                System.out.println("Capacidade: " + e.getCapacidade());
+                System.out.println("----------------------------------");
+            }
+
+        } catch (Exception e) {
+            System.err.println("Erro ao listar eventos: " + e.getMessage());
+        }
+    }
         //es.criarEvento("GamesCon", "Encontro de jogadores", "22-05-2025", "Sao Paulo", 1, 50);
+        
         //es.editarEvento(0, null, null, null, null, null);
         //es.associarPalestrante(0, 0);
         //es.excluirEvento(0);
         
-        InscricaoService is = new InscricaoService();
-        is.inscreverParticipante(1, 1);
+       //InscricaoService is = new InscricaoService();
+        //is.inscreverParticipante(1, 1);
         //is.excluirInscricao(0, 0);
         //is.solicitarCertificado(0, 0); 
 
         //PalestranteService ps2 = new PalestranteService();
         //ps2.inserir("Matheus", "Empresario, full stack e gamer", "Tecnologia");
     }
-}
+
   
 
     
